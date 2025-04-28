5ced1539d54205bfc0d737b5e42e288063b298c2
raza2004 <owaisraza3274@gmail.com>
2025-04-28 23:47:20 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://bod-test-task-client-8nnkl5n9w-owais-projects-7419f1cc.vercel.app/', // your frontend URL
+    origin: 'https://bod-test-task-client-8nnkl5n9w-owais-projects-7419f1cc.vercel.app', // your frontend URL
     credentials: true,
   });
   const port = process.env.PORT || 8000;
